b7492875993d853eb82f4949e858a397cfadbb22
Santiago Moreno <samorenoq@eafit.edu.co>
2021-08-14 17:31:09 -0500

    Initial client-server communication ready

--- Client/client.py
+++ Client/client.py
@@ -1,22 +1,46 @@
-#Import libraries for socket communicaton and constants
+# Import libraries for socket communicaton and constants
 
-import socket, client_constants
+import socket
+import client_constants
 
-#Define new socket connection object for the client
+# Define new socket connection object for the client
 client_socket = socket.socket()
 
 
-#Start the client
+# Start the client
 def execute_client():
-    client_port_tuple = (client_constants.CLIENT_ADDRESS, client_constants.PORT)
+    # This tuple contains the server port
+    client_port_tuple = (client_constants.SERVER_ADDRESS,
+                         client_constants.SERVER_PORT)
     client_socket.connect(client_port_tuple)
-    local_tuple = client_socket.getsockname()
-    print(local_tuple)
+    # This tuple contains the client port
+    client_connection_tuple = client_socket.getsockname()
+    print(
+        f'Connected to server on address {client_connection_tuple[0]} from port {client_connection_tuple[1]}\n')
+
+    # Handle user inputs
+    handle_inputs()
 
 
-#Handle client prompts and commands
+# Handle client prompts and commands
 def handle_inputs():
-    pass
+    message_prompt = "Enter a message (a-z or A-Z only) or press '0' to exit: "
+    message = ''
+    while message != str(client_constants.EXIT):
+        message = input(message_prompt)
+
+        #Check that message is not empty
+        if message == '':
+            print('Empty message! Please try again...')
+        else:
+            client_socket.send(bytes(message, client_constants.ENCODING_FORMAT))
+            response = client_socket.recv(client_constants.RECV_BUFFER_SIZE)
+            print(
+                f'Server response: {response.decode(client_constants.ENCODING_FORMAT)}\n')
+
+    # If the user exits the program
+    print('Exiting...')
+    client_socket.close()
 
 
 def main():
--- StringReader/string_reader.py
+++ StringReader/string_reader.py
@@ -1,39 +1,76 @@
-#Import libraries for socket communication, threading, and server_constants
+# Import libraries for socket communication, threading, and server_constants
 
-import server_constants, socket, threading
+import server_constants
+import socket
+import threading
 
-#Define new socket connection object for the client
+# Define new socket connection object for the client
 server_socket = socket.socket()
 server_address = server_constants.SERVER_ADDRESS
+encoding = server_constants.ENCODING_FORMAT
+port = server_constants.SERVER_PORT
+buff_size = server_constants.RECV_BUFFER_SIZE
 
 
-#Start the server
+# Start the server
 def execute_server():
-    server_port_tuple = (server_address, server_constants.SERVER_PORT)
+    server_port_tuple = (server_address, port)
     server_socket.bind(server_port_tuple)
-    #Allow five unaccepted connections before refusing new connections
+    # Allow five unaccepted connections before refusing new connections
     server_socket.listen(5)
-    print(f'Socket is listening on port {server_constants.SERVER_PORT}...')
+    print(f'Socket is listening on port {port}...')
 
-    #Start handling client messages
+    # Start handling client messages
     while True:
         client_connection, client_address = server_socket.accept()
-        print(f'{client_address[0]} has connected on port {str(client_address[1])}')
-        #Create a new thread to take care of each new client
-        client_thread = threading.Thread(target=handle_client, args=(client_connection, client_address))
+        print(
+            f'{client_address[0]} has connected using their port {str(client_address[1])}')
+        # Create a new thread to take care of each new client
+        client_thread = threading.Thread(
+            target=handle_client, args=(client_connection, client_address))
         client_thread.start()
     print('Closing socket...')
     server_socket.close()
 
 
-#Functionality for handling client messages
-def handle_client(connection, address):
-    print("Handling client...")
+# Functionality for handling client messages
+def handle_client(client_connection: socket.socket, client_address: tuple) -> None:
+    client_is_connected = True
+
+    while client_is_connected:
+        client_message = str(client_connection.recv(
+            buff_size).decode(encoding))
+
+        # If the client wants to exit
+        if client_message == str(server_constants.EXIT):
+            response = '10 - QUIT\n'
+            client_connection.sendall(response.encode(
+                encoding
+            ))
+            client_is_connected = False
+
+        # If the client's message contains invalid characters
+        elif not client_message.isalpha():
+            response = '20 - INVALID CHARACTERS\n'
+            send_to_client(client_connection, response)
+
+        # Otherwise, the client's message is ok
+        else:
+            response = '30 - MSG OK -> TRANSFERRING'
+            send_to_client(client_connection, response)
+
+    print(
+        f'Client with address {client_address[0]} on port {client_address[1]} disconnected')
+    client_connection.close()
+
+
+# Function to define behavior for sending messages to the client
+def send_to_client(client_connection: socket.socket, msg: str) -> None:
+    client_connection.sendall(msg.encode(encoding))
 
 
 def main():
     execute_server()
-
 
 
 if __name__ == "__main__":
